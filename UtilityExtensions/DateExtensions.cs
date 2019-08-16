24f03defad94a9b3f4df927b06735b32bb0c4ea6
Joseph Ajileye <kunlesymls@gmail.com>
2019-08-16 23:24:04 +0100

    Adding more extension methods for utility

---
+++ UtilityExtensions/DateExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class DateExtensions
+    {
+        public static string ToLocalFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd MMM yyyy");
+        }
+
+    }
+}
--- UtilityExtensions/StringExtensions.cs
+++ UtilityExtensions/StringExtensions.cs
@@ -5,23 +5,59 @@ namespace System
     public static class StringExtensions
     {
         /// <summary>
-        /// By default strings are converted to uppercase unless isUpper argument is set to false.
+        /// This method converts a string to Upper case and trim all White spaces
         /// </summary>
         /// <param name="stringValue"></param>
-        /// <param name="isUpper">Optional Parameter</param>
         /// <returns></returns>
-        public static string ToMyString(this string stringValue, bool isUpper = true)
+        public static string ToUpperString(this string stringValue)
         {
             if (!string.IsNullOrEmpty(stringValue))
             {
                 stringValue = stringValue.Trim();
-                return isUpper ? stringValue.ToUpper() : stringValue.First().ToString().ToUpper() + stringValue.Substring(1).ToLower();
+                return stringValue.ToUpper();
             }
             else
             {
                 return null;
             }
         }
+
+        /// <summary>
+        /// This method converts a string to Lower case and trim all White spaces
+        /// </summary>
+        /// <param name="stringValue"></param>
+        /// <returns></returns>
+        public static string ToLowerString(this string stringValue)
+        {
+            if (!string.IsNullOrEmpty(stringValue))
+            {
+                stringValue = stringValue.Trim();
+                return stringValue.ToLower();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// This method converts a string to Sentence case and trim all White spaces
+        /// </summary>
+        /// <param name="stringValue"></param>
+        /// <returns></returns>
+        public static string ToSentenseString(this string stringValue)
+        {
+            if (!string.IsNullOrEmpty(stringValue))
+            {
+                stringValue = stringValue.Trim();
+                return stringValue.First().ToString().ToUpper() + stringValue.Substring(1).ToLower();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
 
     }
 }
